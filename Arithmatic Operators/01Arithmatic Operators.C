bbf81474c4f1f47795037cb55ce8ceeae03b373f
shitaldesai6655 <85870692+shitaldesai6655@users.noreply.github.com>
2021-06-20 21:41:30 +0530

    Add files via upload

---
+++ Arithmatic Operators/01Arithmatic Operators.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int No = 21, Res = 0;
+
+     Res = Res + No;
+
+     printf("\n Result = %d.",Res);
+
+     Res = Res - 11;
+
+     printf("\n Result = %d.",Res);
+
+     Res = Res * 10;
+
+     printf("\n Result = %d.",Res);
+
+
+     getch();
+     return 0;
+}
