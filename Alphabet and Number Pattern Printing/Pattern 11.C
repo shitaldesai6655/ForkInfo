ca993429a6bcec9672db84ba98a3f4b0d325cfcb
shitaldesai6655 <85870692+shitaldesai6655@users.noreply.github.com>
2021-07-06 11:37:51 +0530

    Add files via upload

---
+++ Alphabet and Number Pattern Printing/Pattern 11.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int i = 0, j = 0, x = 0;
+        char ch = 'Q';
+
+        printf("\n Enter columns and rows value for Pattern =  ");
+        scanf("%d",&x);
+
+        for(i = 1; i <= x; i++)
+        {
+                for(j = 1; j <= x; j++)
+                {
+                        if(i >= j)
+                        {
+                                printf("%c", ch);
+                        }
+                        else
+                        {
+                                printf("   ");
+                        }
+                }
+
+                printf("\n");
+        }
+
+        printf("\n Thanks...");
+
+        getch();
+        return 0;
+}
+
---
+++ Alphabet and Number Pattern Printing/Pattern 2.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int i = 0, j = 0, x = 0;
+        char ch = 'A';
+
+        printf("\n Enter columns and rows value for Pattern =  ");
+        scanf("%d",&x);
+
+        for(i = 1; i <= x; i++)
+        {
+                for(j = 1; j <= x; j++, ch++)
+                {
+                        if(i >= j)
+                        {
+                                printf("%c", ch);
+                        }
+                        else
+                        {
+                                printf("   ");
+                        }
+                }
+
+                printf("\n");
+        }
+
+        printf("\n Thanks...");
+
+        getch();
+        return 0;
+}
