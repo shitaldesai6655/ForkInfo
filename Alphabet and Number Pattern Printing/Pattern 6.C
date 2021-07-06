8495a867eafe03a0139e45c616ed424f29553761
shitaldesai6655 <85870692+shitaldesai6655@users.noreply.github.com>
2021-07-06 17:42:18 +0530

    Add files via upload

---
+++ Alphabet and Number Pattern Printing/Pattern 6.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int i = 0, j = 0, x = 0;
+        int no = 0;
+
+        printf("\n Enter columns and rows value for Pattern =  ");
+        scanf("%d",&x);
+
+        for(i = 1; i <= x; i++)
+        {
+                for(j = 1; j <= x; j++)
+                {
+                        if(i >= j)
+                        {
+                                no++;
+                                printf(" %d ",no);
+
+                        }
+                        else
+                        {
+                                printf("   ");
+                        }
+                }
+
+                printf("\n");
+        }
+
+        printf("\n Thanks...");
+
+        getch();
+        return 0;
+}
